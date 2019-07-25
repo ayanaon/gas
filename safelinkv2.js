d3033de0a00f6072956e36e34af8c2f3c91ec02a
ayanaon <53060031+ayanaon@users.noreply.github.com>
2019-07-25 22:41:03 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -19,18 +19,17 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*9)+ 1);
+var x=Math.floor((Math.random()*8)+ 1);
 var xxx=null;
-if(x=="1"){xxx="prior-authorization-cure-with-decision.html"}
-if(x=="2"){xxx="health-insurance-race-against-clock.html"}
-if(x=="3"){xxx="shopping-for-new-health-or-dental.html"}
-if(x=="4"){xxx="are-you-ready-to-rethink-major-medical.html"}
-if(x=="5"){xxx="how-to-make-good-flyer.html"}
-if(x=="6"){xxx="reasons-why-youre-better-off-with.html"}
-if(x=="7"){xxx="is-office-junior-looking-after-your.html"}
-if(x=="8"){xxx="bringing-your-sales-potential-to-far.html"}
-if(x=="9"){xxx="today-we-write-stories-that-you-can.html"}
-a_to_ve[i].href="https://www.upil.me/2019/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="cara-merawat-wajah-agar-awet-muda.html"}
+if(x=="2"){xxx="cara-mencerahkan-wajah-secara-alami.html"}
+if(x=="3"){xxx="cara-memakai-make-up-natural.html"}
+if(x=="4"){xxx="cara-jitu-diet-sehat-wanita-dengan.html"}
+if(x=="5"){xxx="cara-agar-kecantikanmu-tidak-pudar.html"}
+if(x=="6"){xxx="cantik-alami-tanpa-make-up.html"}
+if(x=="7"){xxx="tips-menjaga-kecantikan.html"}
+if(x=="8"){xxx="cara-ampuh-diet-sehat-dan-cepat-tanpa-obat.html"}
+output.value = "https://rankpaypromocode.blogspot.com/2019/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
